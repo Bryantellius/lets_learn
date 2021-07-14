4c9ea27502bc1a7b75fcb8eb1cf5c5cf8184b004
Ben <brbryant2639@gmail.com>
2021-07-13 19:28:21 -0500

    Added html with script to intro js

---
+++ intro.js
@@ -0,0 +1,15 @@
+console.log("Script has run");
+
+// Declaring variables and assigning values
+
+// 'const' is declaration keyword for a variable that cannot be reassigned (constant)
+const firstName = "Ben";
+
+// 'let' is declaration keyword for a variable that can be reassigned
+let age = 24;
+let daysOld = age * 365;
+// 'var' is declaration keyword for a variable that can be reassigned (similar to let)
+var city = "Birmingham";
+
+console.log(firstName); // expected to print "Ben"
+console.log(daysOld); // expected to print 24 * 365
